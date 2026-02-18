ef2b7d358495469fcf77acc3f12df1b1c2ce7432
Sheldon Bish <sbish33@gmail.com>
2026-02-18 15:12:02 -0500

    Added triton and langsmith servers along with huggingface model download scripts

---
+++ huggingface/get_models/model_download.sh
@@ -0,0 +1,5 @@
+hf download openai/gpt-oss-20b model.safetensors.index.json --cache-dir /mnt/bulkStorage/triton/models
+hf download Qwen/Qwen3-32B model.safetensors.index.json --cache-dir /mnt/bulkStorage/triton/models
+hf download meta-llama/Llama-3.1-70B-Instruct model.safetensors.index.json --cache-dir /mnt/bulkStorage/triton/models
+hf download nvidia/Llama-3_3-Nemotron-Super-49B-v1_5-NVFP4 model.safetensors.index.json --cache-dir /mnt/bulkStorage/triton/models
+
